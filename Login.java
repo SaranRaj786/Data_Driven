04afa6db1b17fd4888c07cdfc2d394906303c1a6
SaranRaj786 <77319085+SaranRaj786@users.noreply.github.com>
2021-06-18 16:13:26 +0530

    Add files via upload

---
+++ Login.java
@@ -0,0 +1,56 @@
+package first;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Login
+ */
+@WebServlet("/Login")
+public class Login extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Login() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		{
+		response.setContentType("text/html");
+		PrintWriter pw = response.getWriter();
+		String name = request.getParameter("name");
+		String pswd = request.getParameter("pswd");
+		if(name.equalsIgnoreCase("Sundar")&&pswd.equalsIgnoreCase("Pichai")){
+		pw.println("Login Successfully");	
+		}
+		else
+		{
+			pw.println("Incorrect Password");
+		}
+		}
+		doGet(request, response);
+	}
+
+}
